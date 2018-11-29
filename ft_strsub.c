c498a88381fb6a7112c1b12001f7ea62765f3b11
Quentin GIRARD <qgirard@e3r6p11.42.fr>
2018-11-29 19:14:48 +0100

    creation

---
+++ ft_strsub.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qgirard <qgirard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/13 13:29:47 by qgirard           #+#    #+#             */
+/*   Updated: 2018/11/14 16:38:04 by qgirard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (s != NULL)
+	{
+		while (s[i] && i < start)
+			i++;
+		if (!(str = (char *)malloc(sizeof(*str) * (len + 1))))
+			return (NULL);
+		while (j < len)
+		{
+			str[j] = s[i];
+			j++;
+			i++;
+		}
+		str[j] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
